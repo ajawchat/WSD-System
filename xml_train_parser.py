45a3744af6947d28307c358fae7109bd85188a1e
Ajinkya <ajinkya_awchat@yahoo.com>
2015-04-25 21:46:19 -0400

    added code to segregate all the data for each word in one pass

--- xml_train_parser.py
+++ xml_train_parser.py
@@ -12,25 +12,35 @@ if __name__ == "__main__":
     wordsList = []
 
     currWordNow = ""
+    wordData = ""
 
+    # Loop through the file and gather the data for different words
     for line in fileOpen.readlines():
         #print line
         currWord = ""
+
+        # append the data for a word in the same file and process it and save it as word.train & word.test
+        
+
         currWordList = re.findall("<instance id=\"[\w]+.[a-z].[\w]+.[\d]+\"",line)
         if len(currWordList) != 0:
             #print currWordList[0].split('"')[1].split(".") 
             currWord = currWordList[0].split('"')[1].split(".")
             currWord = currWord[0] + "." + currWord[1]
             print currWord
-
-        #check if the currWord == currWordNow
-        if currWord == currWordNow:
-            print "same"
-        elif currWord != currWordNow and currWord != "":
-            print "not same"
+    
+        
+        if currWord != currWordNow and currWord != "":
+            print wordData
+            wordData = ""
             currWordNow = currWord
             print "changed the global comparator to ",currWord
             # create a new file for the currWord.train
             fileWrite = open(currWord+".train","w")
             
+        else:
+            if "lexelt" not in line:
+                wordData += line
+
+    print wordData
             
