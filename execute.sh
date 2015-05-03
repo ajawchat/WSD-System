40946c7e3148b3fde2b652db74b2daab4c36d61f
Ajinkya <ajinkya_awchat@yahoo.com>
2015-05-03 11:29:56 -0400

    executing timbl commands sequentially using a shell script

---
+++ execute.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+module load timbl/2.6.4
+
+timbl -f ./trainingData/activate.v.train -t ./testData/activate.v.test > file
+timbl -f ./trainingData/add.v.train -t ./testData/add.v.test >> file
+timbl -f ./trainingData/appear.v.train -t ./testData/appear.v.test >> file
+timbl -f ./trainingData/argument.n.train -t ./testData/argument.n.test >> file
+timbl -f ./trainingData/arm.n.train -t ./testData/arm.n.test >> file
+timbl -f ./trainingData/ask.v.train -t ./testData/ask.v.test >> file
+timbl -f ./trainingData/atmosphere.n.train -t ./testData/atmosphere.n.test >> file
+timbl -f ./trainingData/audience.n.train -t ./testData/audience.n.test >> file
+timbl -f ./trainingData/bank.n.train -t ./testData/bank.n.test >> file
+timbl -f ./trainingData/begin.v.train -t ./testData/begin.v.test >> file
+timbl -f ./trainingData/climb.v.train -t ./testData/climb.v.test >> file
+timbl -f ./trainingData/decide.v.train -t ./testData/decide.v.test >> file
+timbl -f ./trainingData/degree.n.train -t ./testData/degree.n.test >> file
+timbl -f ./trainingData/difference.n.train -t ./testData/difference.n.test >> file
+timbl -f ./trainingData/different.a.train -t ./testData/different.a.test >> file
+timbl -f ./trainingData/difficulty.n.train -t ./testData/difficulty.n.test >> file
+timbl -f ./trainingData/disc.n.train -t ./testData/disc.n.test >> file
+timbl -f ./trainingData/eat.v.train -t ./testData/eat.v.test >> file
+timbl -f ./trainingData/encounter.v.train -t ./testData/encounter.v.test >> file
+timbl -f ./trainingData/expect.v.train -t ./testData/expect.v.test >> file
+timbl -f ./trainingData/express.v.train -t ./testData/express.v.test >> file
+timbl -f ./trainingData/hear.v.train -t ./testData/hear.v.test >> file
+timbl -f ./trainingData/hot.a.train -t ./testData/hot.a.test >> file
+timbl -f ./trainingData/image.n.train -t ./testData/image.n.test >> file
+timbl -f ./trainingData/important.a.train -t ./testData/important.a.test >> file
+timbl -f ./trainingData/interest.n.train -t ./testData/interest.n.test >> file
+timbl -f ./trainingData/judgment.n.train -t ./testData/judgment.n.test >> file
+timbl -f ./trainingData/lose.v.train -t ./testData/lose.v.test >> file
+timbl -f ./trainingData/mean.v.train -t ./testData/mean.v.test >> file
+timbl -f ./trainingData/miss.v.train -t ./testData/miss.v.test >> file
+timbl -f ./trainingData/note.v.train -t ./testData/note.v.test >> file
+timbl -f ./trainingData/operate.v.train -t ./testData/operate.v.test >> file
+timbl -f ./trainingData/organization.n.train -t ./testData/organization.n.test >> file
+timbl -f ./trainingData/paper.n.train -t ./testData/paper.n.test >> file
+timbl -f ./trainingData/party.n.train -t ./testData/party.n.test >> file
+timbl -f ./trainingData/performance.n.train -t ./testData/performance.n.test >> file
+timbl -f ./trainingData/plan.n.train -t ./testData/plan.n.test >> file
+timbl -f ./trainingData/play.v.train -t ./testData/play.v.test >> file
+timbl -f ./trainingData/produce.v.train -t ./testData/produce.v.test >> file
+timbl -f ./trainingData/provide.v.train -t ./testData/provide.v.test >> file
+timbl -f ./trainingData/receive.v.train -t ./testData/receive.v.test >> file
+timbl -f ./trainingData/remain.v.train -t ./testData/remain.v.test >> file
+timbl -f ./trainingData/rule.v.train -t ./testData/rule.v.test >> file
+timbl -f ./trainingData/shelter.n.train -t ./testData/shelter.n.test >> file
+timbl -f ./trainingData/simple.a.train -t ./testData/simple.a.test >> file
+timbl -f ./trainingData/smell.v.train -t ./testData/smell.v.test >> file
+timbl -f ./trainingData/solid.a.train -t ./testData/solid.a.test >> file
+timbl -f ./trainingData/sort.n.train -t ./testData/sort.n.test >> file
+timbl -f ./trainingData/source.n.train -t ./testData/source.n.test >> file
+timbl -f ./trainingData/suspend.v.train -t ./testData/suspend.v.test >> file
+timbl -f ./trainingData/talk.v.train -t ./testData/talk.v.test >> file
+timbl -f ./trainingData/treat.v.train -t ./testData/treat.v.test >> file
+timbl -f ./trainingData/use.v.train -t ./testData/use.v.test >> file
+timbl -f ./trainingData/wash.v.train -t ./testData/wash.v.test >> file
+timbl -f ./trainingData/watch.v.train -t ./testData/watch.v.test >> file
+timbl -f ./trainingData/win.v.train -t ./testData/win.v.test >> file
+timbl -f ./trainingData/write.v.train -t ./testData/write.v.test >> file
+
+
+
